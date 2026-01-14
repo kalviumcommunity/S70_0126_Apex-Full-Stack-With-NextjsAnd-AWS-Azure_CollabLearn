01468af6bdeb22380cefc09a69d8e77833ae4ae6
vedha-333 <vedantpandilwad@gmail.com>
2026-01-14 17:00:38 +0530

    auth-login-register

---
+++ backend/server/apps/api/auth/register/route.js
@@ -0,0 +1,8 @@
+var R=require("../../../../chunks/[turbopack]_runtime.js")("server/app/api/auth/register/route.js")
+R.c("server/chunks/1ade8_next_6f4d97cb._.js")
+R.c("server/chunks/1ade8_zod_v4_71a4e8df._.js")
+R.c("server/chunks/[root-of-the-server]__be2d5659._.js")
+R.c("server/chunks/[root-of-the-server]__30ef948a._.js")
+R.c("server/chunks/41486__next-internal_server_app_api_auth_register_route_actions_8ff1aff2.js")
+R.m("[project]/student-engagement-app/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/student-engagement-app/app/api/auth/register/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
+module.exports=R.m("[project]/student-engagement-app/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/student-engagement-app/app/api/auth/register/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
